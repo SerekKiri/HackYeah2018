a20966231d7845bb57264d61eabeee6a243ab08f
Bjornskjald <github@bjorn.ml>
2018-11-25 01:28:37 +0100

    ddd

--- app/lib/screens/redeem_screen.dart
+++ app/lib/screens/redeem_screen.dart
@@ -43,7 +43,7 @@ class RedeemScreen extends StatelessWidget {
                 return ListView.builder(
                   padding: const EdgeInsets.fromLTRB(1.0, 5.0, 1.0, 5.0),
                   itemBuilder: (context, i) {
-                    return _buildApp(i, model);
+                    return _buildApp(context, i, model);
                   },
                   itemCount: model.remoteApps.length,
                 );
@@ -55,7 +55,7 @@ class RedeemScreen extends StatelessWidget {
     );
   }
 
-  Widget _buildApp(int index, AppCardsModel model) {
+  Widget _buildApp(BuildContext context, int index, AppCardsModel model) {
     return Card(
       child: Column(
         mainAxisSize: MainAxisSize.min,
@@ -65,7 +65,18 @@ class RedeemScreen extends StatelessWidget {
             title: Row(
               children: [
                 Expanded(child: Text(model.remoteApps[index].friendlyName)),
-                ButtonTheme(minWidth: 0.0, child: FlatButton(child: Text('REDEEM POINTS', style: TextStyle(color: Colors.green[700])), onPressed: () {  })),
+                ButtonTheme(minWidth: 0.0, child: FlatButton(
+                  child: Text('REDEEM POINTS', style: TextStyle(color: Colors.green[700])),
+                  onPressed: () async {
+                    var id = model.remoteApps[index].id;
+                    var value = await showPointsChoiceDialog(context);
+                    print('ID:');
+                    print(id);
+                    print('Value:');
+                    print(value);
+                    await api.redeemPoints(id, value);
+                  }
+                )),
               ]
             )
           ),
@@ -73,4 +84,41 @@ class RedeemScreen extends StatelessWidget {
       ),
     );
   }
+}
+
+Future<int> showPointsChoiceDialog (context) {
+  return showDialog(
+    context: context,
+    builder: (context) {
+      return SimpleDialog(
+        title: const Text('Select assignment'),
+        children: <Widget>[
+          SimpleDialogOption(
+            onPressed: () { Navigator.pop(context, 5); },
+            child: const Text('5'),
+          ),
+          SimpleDialogOption(
+            onPressed: () { Navigator.pop(context, 10); },
+            child: const Text('10'),
+          ),
+          SimpleDialogOption(
+            onPressed: () { Navigator.pop(context, 15); },
+            child: const Text('15'),
+          ),
+          SimpleDialogOption(
+            onPressed: () { Navigator.pop(context, 30); },
+            child: const Text('30'),
+          ),
+          SimpleDialogOption(
+            onPressed: () { Navigator.pop(context, 60); },
+            child: const Text('60'),
+          ),
+          SimpleDialogOption(
+            onPressed: () { Navigator.pop(context, 120); },
+            child: const Text('120'),
+          ),
+        ],
+      );
+    }
+  );
 }
