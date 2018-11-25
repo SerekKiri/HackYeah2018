ae8e6556c758106b2d25167f0d9a7f9c5fd4015d
NNeast <zawislaktomasz4@gmail.com>
2018-11-25 04:23:29 +0100

    ddd8: add showing icons

--- app/lib/widgets/item.dart
+++ app/lib/widgets/item.dart
@@ -13,12 +13,29 @@ class ItemWidget extends StatelessWidget {
             padding: EdgeInsets.all(8.0),
             child: Row(
               children: <Widget>[
+                 this.iconForId(model.remoteApps[index].appIndentifier),
+                
                 Expanded(
                     child: Text(model.remoteApps[index].friendlyName,
                         style: TextStyle(fontSize: 16.0))),
                 Text(model.remoteApps[index].costPerMinute.toString(),
-                    style: TextStyle(fontSize: 16.0))
+                    style: TextStyle(fontSize: 16.0)),
+                
               ],
-            )));
+      )));
+  }
+
+  Widget iconForId(String pkgName) {
+    var ddd = model.localApps.toList().firstWhere((e) {
+      return e.packageName == pkgName;
+    }, orElse: () => null);
+    if (ddd != null) {
+      return Container(
+                  width: 50.0,
+                  height: 50.0,
+                  padding: EdgeInsets.only(right: 5.0),
+                  child:ddd.icon);
+    } else
+    return Container();
   }
 }
