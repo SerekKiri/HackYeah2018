5ed9e69312cfdfec08e2192432262bed6205915c
Bjornskjald <github@bjorn.ml>
2018-11-25 01:12:32 +0100

    Add id to app model

--- app/lib/models/app.dart
+++ app/lib/models/app.dart
@@ -1,15 +1,13 @@
-class AppsList {
-  List<App> apps;
-}
-
 class App {
+  final int id;
   final int costPerMinute;
   final String appIndentifier;
   final String friendlyName;
 
 
-    App.fromJson(Map<String, dynamic> json)
-      : costPerMinute = json['costPerMinute'],
-        friendlyName = json['friendlyName'],
-        appIndentifier = json['appIndentifier'];
+  App.fromJson(Map<String, dynamic> json)
+    : id = json['id'],
+      costPerMinute = json['costPerMinute'],
+      friendlyName = json['friendlyName'],
+      appIndentifier = json['appIndentifier'];
 }
